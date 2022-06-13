6bbc0b8c27ceed6ee948ff19103f74b740d129d6
Aliaksandr_Maslo@epam.com <Djk4Fyrf11>
2022-06-13 13:38:24 +0300

    improvements

--- jmp.cloud.bank.impl/src/main/java/com/epam/cloud/bank/CreditCardFactory.java
+++ jmp.cloud.bank.impl/src/main/java/com/epam/cloud/bank/CreditCardFactory.java
@@ -5,12 +5,14 @@ import com.epam.dto.CreditBankCard;
 import com.epam.dto.User;
 
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class CreditCardFactory implements Function<User, BankCard> {
 
     @Override
     public BankCard apply(User user) {
-        return new CreditBankCard(UUID.randomUUID().toString(), user);
+        BiFunction<String, User, CreditBankCard> creditBankCardBiFunction = CreditBankCard::new;
+        return creditBankCardBiFunction.apply(UUID.randomUUID().toString(), user);
     }
 }
--- jmp.cloud.bank.impl/src/main/java/com/epam/cloud/bank/DebitBankCardFactory.java
+++ jmp.cloud.bank.impl/src/main/java/com/epam/cloud/bank/DebitBankCardFactory.java
@@ -5,12 +5,14 @@ import com.epam.dto.DebitBankCard;
 import com.epam.dto.User;
 
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class DebitBankCardFactory implements Function<User, BankCard> {
 
     @Override
     public BankCard apply(User user) {
-        return new DebitBankCard(UUID.randomUUID().toString(), user);
+        BiFunction<String, User, DebitBankCard> debitBankCardBiFunction = DebitBankCard::new;
+        return debitBankCardBiFunction.apply(UUID.randomUUID().toString(), user);
     }
 }
